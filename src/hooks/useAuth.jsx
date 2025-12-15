cd6ea4eb6da7120832fb46fc22312218a578913a
Aditta Chakraborty <cseaditta@gmail.com>
2025-12-16 02:46:44 +0600

    use auth hook updated

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -2,8 +2,7 @@ import { useContext } from "react";
 import AuthContext from "../Contexts/AuthContexts/AuthContext.jsx";
 
 const useAuth = () => {
-    const authInfo = useContext(AuthContext);
-    return authInfo;
+    return useContext(AuthContext);
 }
 
 export default useAuth;
